a3c71a2bf60790d30329c102ed841898b3bb04c6
政企-赵伟 <1174952035@qq.com>
2022-05-22 11:31:35 +0800

    first commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,11 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme";
+
+export default defineUserConfig({
+  lang: "zh-CN",
+  title: "主题演示",
+  port:816,
+  description: "vuepress-theme-hope 的演示",
+  base: "/",
+  theme,
+});
